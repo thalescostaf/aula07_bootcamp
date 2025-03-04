3d28fc7bb203917e7eeae9f44b13ee0ec1c6e6ca
Thales Fernandes <contato.thales@gmail.com>
2025-03-04 19:16:03 -0300

    Atualizando com funções da calculadora

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,44 +1,59 @@
-def imprima_o_print():
-    print('Esse é o print da função')
+import os
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    exibir_opcoes()
+
+def exibir_opcoes():
+    print('1. Somar')
+    print('2. Subtrair')
+    print('3. Multiplicar')
+    print('4. Dividir')
+
+    escolha_usuario: int = int(input('Escolha uma opção: '))
+
+    if escolha_usuario == 1:
+        somar_numero(numero_usuario_01,numero_usuario_02)
+    elif escolha_usuario == 2:
+        substrair_numero(numero_usuario_01,numero_usuario_02)
+    elif escolha_usuario == 3:
+        multiplicar_numero(numero_usuario_01,numero_usuario_02)
+    elif escolha_usuario == 4:
+        dividir_numero(numero_usuario_01,numero_usuario_02)
+    else:
+        print('Opção invalida!')
 
 def somar_numero(nun1, nun2):
     valor1: float = nun1
     valor2: float = nun2
     valor3: float = valor1 + valor2
 
-    return valor3
+    return print(f'A soma dos números é: ', valor3)  
 
 def substrair_numero(nun1, nun2):
     valor1: float = nun1
     valor2: float = nun2
     valor3: float = nun1 - nun2
 
-    return valor3
+    return print(f'A subtração dos números é: ', valor3)  
 
 def dividir_numero(nun1, nun2):
     valor1: float = nun1
     valor2: float = nun2
     valor3: float = nun1 / nun2
 
-    return valor3    
+    return print(f'A divisão dos números é: ', valor3)  
 
 def multiplicar_numero(nun1, nun2):
     valor1: float = nun1
     valor2: float = nun2
     valor3: float = nun1 * nun2
 
-    return valor3    
+    return print(f'A multiplicação dos números é: ', valor3)  
 
-numero_usuario_01: float = float(input('Digite um numero'))
-numero_usuario_02: float = float(input('Digite outro numero'))
-
-imprima_o_print()
-fazer_soma = somar_numero(numero_usuario_01,numero_usuario_02)
-fazer_subtracao = substrair_numero(numero_usuario_01, numero_usuario_02)
-fazer_divisao = dividir_numero(numero_usuario_01, numero_usuario_02)
-fazer_multiplicacao = multiplicar_numero(numero_usuario_01, numero_usuario_02)
-
-print(f"A soma dos números é: ", fazer_soma)
-print(f"A subtração dos números é: ", fazer_subtracao)
-print(f"A divisão dos números é: ", fazer_divisao)
-print(f"A multiplicação dos números é: ", fazer_multiplicacao)
+# Declaração para iniciar o aplicativo
+if __name__ == '__main__':
+    numero_usuario_01: float = float(input('Digite um numero'))
+    numero_usuario_02: float = float(input('Digite outro numero'))
+    main()
+    
